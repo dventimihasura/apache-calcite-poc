e673c0105d358f2ea38025727f09b382c4d2e669
David A. Ventimiglia <davidaventimiglia@hasura.io>
2024-08-21 13:35:32 -0700

    Added the JDBC code

--- src/main/java/io/hasura/App.java
+++ src/main/java/io/hasura/App.java
@@ -1,13 +1,17 @@
 package io.hasura;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+import java.sql.*;
+
+public class App {
+    public static void main( String[] args ) throws SQLException {
+	if (System.getProperty("jdbc:calcite:model") != null)
+	    try (
+		var con = DriverManager.getConnection(String.format("jdbc:calcite:model=%s", System.getProperty("jdbc:calcite:model")));
+		var stm = con.createStatement();
+		var res = stm.executeQuery(args[1]);) {
+		var met = res.getMetaData();
+		while (res.next())
+		    for (int i = 1; i<=met.getColumnCount(); i++)
+			System.out.print(String.format("%s,", res.getString(i)));}
+	    catch (SQLException e) {
+		e.printStackTrace(System.err);}}}
